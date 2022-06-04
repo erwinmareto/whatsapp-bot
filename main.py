09e1f1eeedd3c744ef0d267c65d61cfc060f9cd0
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2022-06-04 23:41:48 +0700

    add download

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 url = "https://web.whatsapp.com/"
@@ -21,7 +23,7 @@ for contact in chat_boxes:
 papa.click()
 
 
-time.sleep(60)
+time.sleep(30)
 
 for i in range(2):
     driver.execute_script("window.scrollTo(0, -1080)")
@@ -30,8 +32,42 @@ time.sleep(10)
 
 links = driver.find_elements_by_css_selector("._33LGR a")
 
+download_links = []
+
 for link in links:
-    # if links.index(link) % 2 != 0:
-    #     print(link.text)
+    if links.index(link) % 2 == 0:
+        download_links.append(link.text)
+        # print(link.text)
 
     print(link.text)
+print(download_links)
+
+
+for down_link in download_links:
+    driver.get("https://getx.topsandtees.space/PXAeoljYgO")
+
+    time.sleep(5)
+
+    if "youtu.be" in down_link:
+
+        text_field = driver.find_element_by_xpath("/html/body/main/div/section/form/div/input[1]")
+        text_field.send_keys(down_link)
+        text_field.send_keys(Keys.ENTER)
+
+        driver.switch_to.window(driver.window_handles[0])
+        download_button = driver.find_element_by_xpath("/html/body/main/div/section/div[1]/div[1]/div/a")
+        download_button.click()
+
+        time.sleep(20)
+
+        try:
+            down_button = driver.find_element_by_xpath("/html/body/main/div/div/p/span")
+            down_button.click()
+        except NoSuchElementException:
+            time.sleep(20)
+            down_button = driver.find_element_by_xpath("/html/body/main/div/div/p/span")
+            down_button.click()
+
+        print(down_link)
+
+
